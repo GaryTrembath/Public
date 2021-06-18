039fa584311ad734bf9446fff9d01d10755cdf6e
GaryTrembath <37798696+GaryTrembath@users.noreply.github.com>
2021-06-18 21:09:02 +0100

    Update CTX_Extention.ps1

--- WelshGovernment/PowershellScripts/CTX_Extention.ps1
+++ WelshGovernment/PowershellScripts/CTX_Extention.ps1
@@ -1,1 +1,3 @@
-New-Item 
+ new-item -ItemType Directory -Path 'C:\\PowerShellScripts\\' 
+ Invoke-WebRequest -uri 'https://github.com/GaryTrembath/Public/blob/main/WelshGovernment/PowershellScripts/CTX_Extention.ps1' -OutFile 'C:\\PowerShellScripts\\CTX_Extention.ps1' 
+
